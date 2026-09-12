d23df3be0fd6f2591c8c9588d9d4462974a00908
Hyper <34012267+hyperbx@users.noreply.github.com>
2026-09-12 16:15:58 +0100

    CLI: only import vswhere module on Windows

--- tools/scripts/common/vs.py
+++ tools/scripts/common/vs.py
@@ -1,23 +1,32 @@
+import platform
 from common.error import error
 from common.version import version, version_pattern
 
-try:
+g_is_windows = platform.system().lower().startswith("win")
+
+if g_is_windows:
 #
-    import vswhere
-#
-except ImportError:
-#
-    exit(error.MISSING_PACKAGES)
+    try:
+    #
+        import vswhere
+    #
+    except ImportError:
+    #
+        exit(error.MISSING_PACKAGES)
+    #
 #
 
 def get_latest_version() -> version:
 #
     """Gets the latest version of Visual Studio."""
-    
-    installation_version = vswhere.find(prerelease = True, latest = True, prop = "installationVersion")
 
-    if installation_version:
-        return version.from_str(installation_version[0], version_pattern.MSVER)
+    if g_is_windows:
+    #
+        installation_version = vswhere.find(prerelease = True, latest = True, prop = "installationVersion")
+
+        if installation_version:
+            return version.from_str(installation_version[0], version_pattern.MSVER)
+    #
 
     return version(0, 0, 0, 0)
 #
